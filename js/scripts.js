bd71cf8964f1db578ee538e4753d51207da71f0f
yeshwanthkj <67580069+yeshwanthkj@users.noreply.github.com>
2021-06-05 17:43:15 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,7 @@ function sum_args(num1,num2)
     return(num1+num2);
 }
 sum_args(-27,55)
-document.getElementById("sum_result").innerHTML=sum_args(-27,55);
+
+var order_date =new Date("03/26/2020");
+var delivery_date=new Date("04/02/2020");
+document.getElementById("delivery_time").innerHTML=((delivery_date.getTime()- order_date.getTime())/86400000)+"Days";
